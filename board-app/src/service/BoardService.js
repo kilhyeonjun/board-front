ab3f1b87df7d356adfe371689d8845dbf34a1b25
길현준 <kboxstar@gmail.com>
2021-08-18 18:57:14 +0900

    board pasing

--- board-app/src/service/BoardService.js
+++ board-app/src/service/BoardService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 const BOARD_API_BASE_URL = 'http://localhost:8080/api/board';
 
 class BoardService {
-    getBoards() {
-        return axios.get(BOARD_API_BASE_URL);
+    getBoards(p_num) {
+        return axios.get(BOARD_API_BASE_URL + '?p_num=' + p_num);
     }
     createBoard(board) {
         return axios.post(BOARD_API_BASE_URL, board);
